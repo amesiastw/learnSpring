2598ee092df79bc4e066fac66cfab66f9b4d0f65
alejandrom <amesias@thoughtworks.com>
2019-12-29 09:30:54 -0500

    Alejo Adds annotations for dependency injection

--- src/main/java/com/alejo/learning/dao/FakeDataDao.java
+++ src/main/java/com/alejo/learning/dao/FakeDataDao.java
@@ -1,9 +1,11 @@
 package com.alejo.learning.dao;
 
 import com.alejo.learning.model.User;
+import org.springframework.stereotype.Repository;
 
 import java.util.*;
 
+@Repository
 public class FakeDataDao implements UserDao {
 
     private static Map<UUID, User> database;
--- src/main/java/com/alejo/learning/service/UserService.java
+++ src/main/java/com/alejo/learning/service/UserService.java
@@ -1,11 +1,23 @@
 package com.alejo.learning.service;
 
+import com.alejo.learning.dao.FakeDataDao;
+import com.alejo.learning.dao.UserDao;
 import com.alejo.learning.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
+@Service
 public class UserService {
+
+    private UserDao myUserDao;
+
+    @Autowired
+    public UserService(UserDao userDao) {
+        this.myUserDao = userDao;
+    }
 
     public List<User> getAllUsers() {
         return null;
